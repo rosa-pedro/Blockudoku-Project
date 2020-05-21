b18a87384c3a296e15e8e3044b539f950a9bea87
Pedro Rosa <rosapedro10@gmail.com>
2020-05-21 19:20:33 +0100

    Initial commit.

---
+++ BlockuDoku/src/main/java/com/ips/blockudoku/game/Piece.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ips.blockudoku.game;
+
+/**
+ *
+ * @author rosap
+ */
+public class Piece {
+    
+}
