5c3009b3600c0474cd5dddfac7ad2ee4623dc098
Pedro Rosa <rosapedro10@gmail.com>
2020-05-22 09:18:02 +0100

    Iblock commit.

---
+++ BlockuDoku/src/main/java/game/IBlock.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author rosap
+ */
+public class IBlock extends Piece{
+    
+    public IBlock(){
+        super("A1");
+    }
+    
+    @Override
+    public void buildPiece(){
+        addSquare("A1");
+        addSquare("A2");
+        addSquare("A3");
+    }
+    
+}
